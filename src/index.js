e134a5ca1256e8670c9a318d83469eae943890a1
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-02-07 19:59:56 +1100

    Create exit to page button on add task menu

--- src/index.js
+++ src/index.js
@@ -7,6 +7,23 @@ function createAddTaskMenu() {
     let addTaskMenu = document.createElement('div');
     addTaskMenu.setAttribute('id', 'add-task-menu');
     body.appendChild(addTaskMenu);
+
+    let exitMenuButton = document.createElement('button');
+    exitMenuButton.setAttribute('id', 'exit-menu-button');
+    addTaskMenu.appendChild(exitMenuButton);
+    exitMenu(exitMenuButton);
+}
+
+function removeAddTaskMenu() {
+    let addTaskMenu = document.querySelector('#add-task-menu');
+    addTaskMenu.remove();
+}
+
+function exitMenu(exitMenuButton) {
+    exitMenuButton.addEventListener('click', () => {
+        removeAddTaskMenu();
+        body.classList.remove('blur');
+    });
 }
 
 addTaskButton.addEventListener('click', () => {
